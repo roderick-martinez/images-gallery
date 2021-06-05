859ecb4a5e3c421524fd8b1ce1dc503880bfabe6
Pulpo <devpulpo@gmail.com>
2021-06-05 00:58:11 -0400

    controlled search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
+import Search from './components/Search';
+
 
 
 const App = () => {
+  // state handling
+  // useState returns 2 variables: a string (word) and a function (setWord)
+  // setWord is a function that sets the value of the variable word
+  // both are passed as properties to the Search component so that they can be used by that component
+  const [word, setWord] = useState('');
+  
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(word);
+  }
+
   return (
     <div>
       <Header title="Images Gallery"/>
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
     </div>
   );
 }
