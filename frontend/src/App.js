82f8025805651b4902036e6a6c531ae5786615ce
Pulpo <devpulpo@gmail.com>
2021-06-09 08:50:25 -0400

    display and delete images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
+import ImageCard from './components/ImageCard';
+import { Container, Col, Row } from 'react-bootstrap';
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
@@ -16,11 +18,8 @@ const App = () => {
   // useState sets a default empty array at first.
   const [images, setImages] = useState([]);
 
-  console.log(images);
-
   const handleSearchSubmit = (e) => {
     e.preventDefault();
-    console.log(word);
     fetch(
       `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
     )
@@ -28,7 +27,10 @@ const App = () => {
       .then((data) => {
         // pull data from array using js spread operator ...
         // it will create a new array of images
-        setImages([data, ...images]);
+        // first: create an object (using brackets) and add data array using spread operator
+        // as well as a new  a new field (or is it a variable?) named title that contains the contents of word
+        // second: add contents of images using spread operator
+        setImages([{ ...data, title: word }, ...images]);
       })
       .catch((err) => {
         console.log(err);
@@ -36,10 +38,40 @@ const App = () => {
     setWord('');
   };
 
+  /* 
+    Function that will be passed to the ImagesCard component.
+  */
+  const handleDeleteImage = (id) => {
+    // Use the filter method to search for provided id.
+    // If it matches, then delete it from images array.
+    // The Filter method returns a new array.
+    setImages(images.filter((image) => image.id !== id));
+  };
+
   return (
     <div>
       <Header title="Images Gallery" />
+
+      {/*  
+      Display Search component and pass word prop as well as functions as properties.
+      */}
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+
+      <Container className="mt-4">
+        <Row xs={1} md={2} lg={3}>
+          {/* 
+            Using the JS map method, map each image in the images array and 
+            spit out the JSX that displays the image cards.
+            Technically it's returning an array of ImageCard.
+            The "key" prop provides the JSX code the index of the element in 
+            the images array. This is required by JSX.  */}
+          {images.map((image, i) => (
+            <Col key={i} className="pb-3">
+              <ImageCard image={image} deleteImage={handleDeleteImage} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 };
