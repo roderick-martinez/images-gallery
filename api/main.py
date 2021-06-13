f8b41a9f520a94f0666481e16ce2d86cbbd636af
Pulpo <devpulpo@gmail.com>
2021-06-13 08:09:52 -0400

    changed api in the frontend and enabled CORS in api

--- api/main.py
+++ api/main.py
@@ -3,6 +3,7 @@ import dotenv
 import requests
 from flask import Flask, request
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 load_dotenv(dotenv_path="./.env.local")
 
@@ -14,7 +15,7 @@ if not UNSPLASH_KEY:
   raise EnvironmentError("Please create .env.local file and instert UNSPLASH_KEY")
 
 app = Flask(__name__)
-
+CORS(app)
 app.config["DEBUG"] = DEBUG
 
 @app.route("/new-image")
